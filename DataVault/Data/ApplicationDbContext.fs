93cb0940e36adc574cd3c852b951aeb8da52c102
Soeking <great.from.g@gmail.com>
2025-07-16 23:21:26 +0900

    add package

---
+++ DataVault/Data/ApplicationDbContext.fs
@@ -0,0 +1,8 @@
+namespace DataVault.Data
+
+open Microsoft.AspNetCore.Identity.EntityFrameworkCore
+open Microsoft.EntityFrameworkCore
+
+type ApplicationDbContext(options: DbContextOptions<ApplicationDbContext>) =
+    inherit IdentityDbContext(options)
+    

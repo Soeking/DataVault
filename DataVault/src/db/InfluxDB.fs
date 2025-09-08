34c0eb71e6d1ffd30a8a4a1407c8ac7e792da8e7
Soeking <great.from.g@gmail.com>
2025-09-08 23:42:20 +0900

    add read db

--- DataVault/src/db/InfluxDB.fs
+++ DataVault/src/db/InfluxDB.fs
@@ -1,6 +1,8 @@
 module DataVault.db.InfluxDB
 
+open System.Linq
 open System.Threading
+open System.Threading.Tasks
 open InfluxDB3.Client
 open InfluxDB3.Client.Write
 open System
@@ -62,3 +64,29 @@ let writeStockDataList (stockList: StockDailyData list) seriesName =
         let tags = Map [ "series", seriesName ]
 
         writeStockData client "stock" fields tags st.Date |> Async.RunSynchronously)
+
+let readTable seriesName =
+    task {
+        let list = ResizeArray<StockDailyData>()
+
+        let query =
+            $"SELECT value FROM stock WHERE series = %s{seriesName} ORDER BY time ASC"
+
+        let! result = queryData client query
+
+        let enum =
+            result.GetAsyncEnumerator((new CancellationTokenSource(TimeSpan.FromSeconds(20.0))).Token)
+
+        while! enum.MoveNextAsync() do
+            let timestamp =
+                enum.Current.GetTimestamp()
+                |> Nullable.op_Explicit
+                |> int64
+                |> fun x -> x / 1_000_000L |> DateTimeOffset.FromUnixTimeMilliseconds
+
+            let value = enum.Current.GetDoubleField("value") |> Nullable.op_Explicit |> decimal
+
+            list.Add({ Date = timestamp.Date; Value = value })
+
+        return list.ToArray() |> List.ofArray
+    }
